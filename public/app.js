b9d6228e378787ba07af580c58582f2064f74056
AdrixSC <adriana.be@outlook.com>
2018-03-05 12:50:24 -0600

    accediendo a repos

--- public/app.js
+++ public/app.js
@@ -14,15 +14,29 @@ function getUser(){
     const data = JSON.parse(this.responseText)
     var pics = document.getElementById('pics');
     pics.innerHTML = ''
-    //console.log("data",data)
+    console.log("data",data)
     var photo = document.createElement('img');
-    photo.setAttribute('src', data.avatar_url)
-    var name = document.createElement("p")
+    photo.setAttribute('src', data.avatar_url);
+    var ancourt = document.createElement('a');
+    ancourt.setAttribute('href', data.html_url);
+    ancourt.setAttribute('target', '_blank');
+    var name = document.createElement("p");
     name.innerHTML = data.login;
-    //console.log(name)
+    console.log(name);
+    console.log(ancourt);
     pics.appendChild(name);
+    pics.appendChild(ancourt);
     pics.appendChild(photo);
+
+    photo.addEventListener("click", getRepos);
 };
+
+function getRepos(data) {
+    var ancourt = document.createElement('a');
+    ancourt.setAttribute('href', data.html_url);
+    ancourt.setAttribute('target', '_blank');
+    console.log("si entra funcion")
+}
 
 //iterateUsers(userArray)
 
@@ -31,6 +45,6 @@ button.addEventListener('click', searchUser)
 
 function searchUser(){
     var user = document.getElementById('input-user');
-    //console.log(user)
+    console.log(user)
     getData(user.value)
 }
